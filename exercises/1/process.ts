b5b61abf260f2c3b9c19841ce3e0d74f3711be92
David Hughes <david.hughes@cancer.org.uk>
2023-12-01 16:09:45 +0000

    Day 1: Complete first part

--- exercises/1/process.ts
+++ exercises/1/process.ts
@@ -1,20 +1,36 @@
+const fs = require('node:fs');
+
+const fetchFileContents = (pathToFile: string) => {
+    try {
+        const data = fs.readFileSync(pathToFile, 'utf8');
+        return data.split('\n');
+    } catch (err) {
+        console.error(err);
+        return[];
+    }
+}
+
 const isCharacterNumeric = (char: string) => {
     return /\d/.test(char)
 }
-let calculateCalibrationValue = (line: string): number => {
+let calculateCalibrationValue = (line: string) => {
     const chars = line.split('');
+    if (chars.length === 0) {
+        return 0;
+    }
     const numbers = chars.filter(isCharacterNumeric);
     if (numbers.length > 1) {
         return Number.parseInt(numbers[0] + numbers.pop())
     } else if (numbers.length === 1) {
         return Number.parseInt(numbers[0] + numbers[0]);
     } else {
-        console.log(`Something ain't right!`)
+        console.log(`Something ain't right! Line is: ${line}`)
         return -1;
     }
 }
 
-console.log(calculateCalibrationValue('1abc2'));
-console.log(calculateCalibrationValue('pqr3stu8vwx'));
-console.log(calculateCalibrationValue('a1b2c3d4e5f'));
-console.log(calculateCalibrationValue('treb7uchet'));
+let crunchTheNumbers = (input: string[]) => input.reduce((total, value) => total + calculateCalibrationValue(value), 0)
+
+const input = fetchFileContents('input');
+
+console.log(crunchTheNumbers(input));
