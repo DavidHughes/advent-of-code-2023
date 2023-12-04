54c45fcaf42624e194870c810ca0d25551f50c37
David Hughes <david.hughes@cancer.org.uk>
2023-12-04 09:48:51 +0000

    Fix importer dying on empty line at end of file.

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,9 @@ import fs from 'node:fs';
 export const fetchFileContents = (pathToFile: string) => {
     try {
         const data = fs.readFileSync(pathToFile, 'utf8');
-        return data.split('\n');
+        // The filter is to remove the annoying empty line at end of file that keeps tripping me up!
+        return data.split('\n').filter((value) => value);
+        
     } catch (err) {
         console.error(err);
         return[];
